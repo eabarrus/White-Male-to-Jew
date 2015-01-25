95c4274cd5d507b0b55f7928f20b5b3c18f7d060
Zero-Shift <o0ZeroShift0o@gmail.com>
2015-01-25 13:17:58 -0600

    Bug Fix
    Fixed bug related to order of filtering (I hope

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,16 +31,6 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bWhite Male\b/g, "Jew");
-	v = v.replace(/\bWhite male\b/g, "Jew");
-	v = v.replace(/\bwhite Male\b/g, "Jew");
-	v = v.replace(/\bwhite male\b/g, "Jew");
-	
-	v = v.replace(/\bCis Male\b/g, "Jew");
-	v = v.replace(/\bCis male\b/g, "Jew");
-	v = v.replace(/\bcis Male\b/g, "Jew");
-	v = v.replace(/\bcis male\b/g, "Jew");
-	
 	v = v.replace(/\bCis White Male\b/g, "Jew");
 	v = v.replace(/\bCis White male\b/g, "Jew");
 	v = v.replace(/\bCis white male\b/g, "Jew");
@@ -58,6 +48,16 @@ function handleText(textNode)
 	v = v.replace(/\bWhite cis Male\b/g, "Jew");
 	v = v.replace(/\bwhite cis Male\b/g, "Jew");
 	v = v.replace(/\bwhite Cis Male\b/g, "Jew");
+	
+	v = v.replace(/\bWhite Male\b/g, "Jew");
+	v = v.replace(/\bWhite male\b/g, "Jew");
+	v = v.replace(/\bwhite Male\b/g, "Jew");
+	v = v.replace(/\bwhite male\b/g, "Jew");
+	
+	v = v.replace(/\bCis Male\b/g, "Jew");
+	v = v.replace(/\bCis male\b/g, "Jew");
+	v = v.replace(/\bcis Male\b/g, "Jew");
+	v = v.replace(/\bcis male\b/g, "Jew");
 	
 	textNode.nodeValue = v;
 }
