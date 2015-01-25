18524bc6dc8f1184d2dd37b109037743253b31f5
Zero-Shift <o0ZeroShift0o@gmail.com>
2015-01-25 12:16:56 -0600

    Update content_script.js
    Fundamental change.
    
    "Cloud-to-Butt" is now "White Male-to-Jew" so it's easier to tell how bigoted SJWs can be.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bWhite Male\b/g, "Jew");
+	v = v.replace(/\bWhite male\b/g, "Jew");
+	v = v.replace(/\bwhite Male\b/g, "Jew");
+	v = v.replace(/\bwhite male\b/g, "Jew");
 	
 	textNode.nodeValue = v;
 }
